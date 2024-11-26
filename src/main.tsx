7ec8e48dcaf377f8ae39424993433e64fa553f21
SangGyu Ahn <gd20624@gmail.com>
2024-11-26 14:24:55 +0900

    main.tsx에 redux상태관리에 필요한 provider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import creatorMainRouter from "./routers/creatorMainRouter.tsx";
+import projectStore from "./store.ts";
+import {Provider} from "react-redux";
 
 
 createRoot(document.getElementById('root')!).render(
-
-    <RouterProvider router={creatorMainRouter}></RouterProvider>
-
+    <Provider store={projectStore}>
+        <RouterProvider router={creatorMainRouter}></RouterProvider>
+    </Provider>
 )
